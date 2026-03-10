d769322208255a3d910e26b3c72454c1215fba22
Serj <serj@local>
2026-03-10 21:30:43 +0100

    update

--- main.js
+++ main.js
@@ -944,7 +944,7 @@
 })();
 
 (() => {
-  const creepyBtns = Array.from(document.querySelectorAll(".service-hero-copy .creepy-btn"));
+  const creepyBtns = Array.from(document.querySelectorAll(".service-hero .creepy-btn"));
   if (creepyBtns.length === 0) return;
 
   creepyBtns.forEach((creepyBtn) => {
